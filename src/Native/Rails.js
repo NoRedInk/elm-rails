c5b2978aa5f2619b67dfd88e82e8508da8571e5a
eeue56 <enalicho@gmail.com>
2016-10-31 17:37:20 +0100

    unfurl the logic for getting the token a bit

--- src/Native/Rails.js
+++ src/Native/Rails.js
@@ -1,16 +1,24 @@
 var _NoRedInk$elm_rails$Native_Rails = function(){
-
   var getCsrfToken = function() {
-    // when we aren't in an actual dom
-    if ((typeof window === "undefined") || (typeof process === 'object')){
+    // when we aren't in an actual dom, short curcuit
+    // we can't always trust this test because mocking
+    if (typeof window === "undefined"){
       return { ctor : 'Nothing' };
     }
 
-    var csrfTokenNode = document.head.querySelector('meta[name="csrf-token"]');
-    var csrfToken =
-        (csrfTokenNode === null || (typeof csrfTokenNode.content !== "string"))
-            ? { ctor: 'Nothing' }
-            : { ctor: 'Just', _0: csrfTokenNode.content };
+    var csrfToken = { ctor: 'Nothing' };
+    var csrfTokenNode = null;
+
+    try {
+      csrfTokenNode = document.head.querySelector('meta[name="csrf-token"]');
+    } catch (e){
+      // ignore document-based errors
+    }
+
+    if ((csrfTokenNode !== null) && (typeof csrfTokenNode.content === "string")){
+      csrfToken = { ctor: 'Just', _0: csrfTokenNode.content };
+    }
+
     return csrfToken;
   };
 
