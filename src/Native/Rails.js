e79980924140787058562f820c0226e8ceb8b0be
eeue56 <enalicho@gmail.com>
2016-10-31 15:36:26 +0100

    use a getter to make sure that the meta tag is got after load

--- src/Native/Rails.js
+++ src/Native/Rails.js
@@ -1,18 +1,22 @@
 var _NoRedInk$elm_rails$Native_Rails = function(){
-  // when we aren't in an actual dom
-  if ((typeof window === "undefined") || (typeof process === 'object')){
-    return {
-      csrfToken : { ctor : 'Nothing' }
-    };
-  }
 
-  var csrfTokenNode = document.head.querySelector('meta[name="csrf-token"]');
-  var csrfToken =
-      (csrfTokenNode === null || (typeof csrfTokenNode.content !== "string"))
-          ? { ctor: 'Nothing' }
-          : { ctor: 'Just', _0: csrfTokenNode.content };
+  var getCsrfToken = function() {
+    // when we aren't in an actual dom
+    if ((typeof window === "undefined") || (typeof process === 'object')){
+      return { ctor : 'Nothing' };
+    }
+
+    var csrfTokenNode = document.head.querySelector('meta[name="csrf-token"]');
+    var csrfToken =
+        (csrfTokenNode === null || (typeof csrfTokenNode.content !== "string"))
+            ? { ctor: 'Nothing' }
+            : { ctor: 'Just', _0: csrfTokenNode.content };
+    return csrfToken;
+  };
 
   return {
-    csrfToken: csrfToken
+    get csrfToken () {
+      return getCsrfToken();
+    }
   }
 }();
