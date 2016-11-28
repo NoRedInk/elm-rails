82d52fccc8e4494916cb3a269aed886740c886bc
Richard Feldman <richard.t.feldman@gmail.com>
2016-11-28 13:55:51 -0800

    :art: elm-format

--- src/Rails.elm
+++ src/Rails.elm
@@ -24,8 +24,8 @@ import Native.Rails
 {-| The kinds of errors a Rails server may return.
 -}
 type Error error
-  = HttpError Http.Error
-  | RailsError error
+    = HttpError Http.Error
+    | RailsError error
 
 
 {-| Send a GET request to the given URL. You also specify how to decode the response.
@@ -39,7 +39,7 @@ type Error error
 -}
 get : ResponseDecoder error value -> String -> Task (Error error) value
 get decoder url =
-  fromJson decoder (send "GET" url Http.empty)
+    fromJson decoder (send "GET" url Http.empty)
 
 
 {-| Send a POST request to the given URL. You also specify how to decode the response.
@@ -54,7 +54,7 @@ get decoder url =
 -}
 post : ResponseDecoder error value -> String -> Http.Body -> Task (Error error) value
 post decoder url body =
-  fromJson decoder (send "POST" url body)
+    fromJson decoder (send "POST" url body)
 
 
 {-| Utility for working with Rails. Wraps Http.send, passing an Authenticity Token along with the type of request. Suitable for use with `fromJson`:
@@ -87,54 +87,54 @@ post decoder url body =
 -}
 send : String -> String -> Http.Body -> Task Http.RawError Http.Response
 send verb url body =
-  let
-    csrfTokenString =
-      Maybe.withDefault "" csrfToken
+    let
+        csrfTokenString =
+            Result.withDefault "" csrfToken
 
-    csrfTokenHeaders =
-      if
-        (String.isEmpty csrfTokenString)
-          || ((String.toUpper verb) == "GET")
-      then
-        []
-      else
-        [ "X-CSRF-Token" => csrfTokenString ]
+        csrfTokenHeaders =
+            if
+                (String.isEmpty csrfTokenString)
+                    || ((String.toUpper verb) == "GET")
+            then
+                []
+            else
+                [ "X-CSRF-Token" => csrfTokenString ]
 
-    requestSettings =
-      { verb = verb
-      , headers =
-          csrfTokenHeaders
-            ++ [ "Content-Type" => "application/json"
-               , "Accept" => "application/json, text/javascript, */*; q=0.01"
-               , "X-Requested-With" => "XMLHttpRequest"
-               ]
-      , url = url
-      , body = body
-      }
-  in
-    Http.send Http.defaultSettings requestSettings
+        requestSettings =
+            { verb = verb
+            , headers =
+                csrfTokenHeaders
+                    ++ [ "Content-Type" => "application/json"
+                       , "Accept" => "application/json, text/javascript, */*; q=0.01"
+                       , "X-Requested-With" => "XMLHttpRequest"
+                       ]
+            , url = url
+            , body = body
+            }
+    in
+        Http.send Http.defaultSettings requestSettings
 
 
 {-| JSON Decoders for parsing an HTTP response body.
 -}
 type alias ResponseDecoder error value =
-  { success : Decoder value
-  , failure : Decoder error
-  }
+    { success : Decoder value
+    , failure : Decoder error
+    }
 
 
 {-| Returns a decoder suitable for passing to `fromJson`, which uses the same decoder for both success and failure responses.
 -}
 always : Decoder value -> ResponseDecoder value value
 always decoder =
-  ResponseDecoder decoder decoder
+    ResponseDecoder decoder decoder
 
 
 {-| Returns a decoder suitable for passing to `fromJson`.
 -}
 decoder : Decoder value -> Decoder error -> ResponseDecoder error value
 decoder successDecoder failureDecoder =
-  ResponseDecoder successDecoder failureDecoder
+    ResponseDecoder successDecoder failureDecoder
 
 
 {-| Think `Http.fromJson`, but with additional effort to parse a non-20x response as JSON.
@@ -146,61 +146,61 @@ decoder successDecoder failureDecoder =
 -}
 fromJson : ResponseDecoder error value -> Task Http.RawError Http.Response -> Task (Error error) value
 fromJson decoder response =
-  let
-    onSuccess response str =
-      case decodeString decoder.success str of
-        Ok v ->
-          succeed v
+    let
+        onSuccess response str =
+            case decodeString decoder.success str of
+                Ok v ->
+                    succeed v
 
-        Err msg ->
-          fail (HttpError <| Http.UnexpectedPayload str)
+                Err msg ->
+                    fail (HttpError <| Http.UnexpectedPayload str)
 
-    onError response str =
-      case decodeString decoder.failure str of
-        Ok v ->
-          fail (RailsError v)
+        onError response str =
+            case decodeString decoder.failure str of
+                Ok v ->
+                    fail (RailsError v)
 
-        Err msg ->
-          fail (HttpError <| Http.BadResponse response.status response.statusText)
+                Err msg ->
+                    fail (HttpError <| Http.BadResponse response.status response.statusText)
 
-    promoteError rawError =
-      case rawError of
-        Http.RawTimeout ->
-          HttpError Http.Timeout
+        promoteError rawError =
+            case rawError of
+                Http.RawTimeout ->
+                    HttpError Http.Timeout
 
-        Http.RawNetworkError ->
-          HttpError Http.NetworkError
-  in
-    mapError promoteError response
-      `andThen` handleResponse onSuccess onError
+                Http.RawNetworkError ->
+                    HttpError Http.NetworkError
+    in
+        mapError promoteError response
+            `andThen` handleResponse onSuccess onError
 
 
 type alias ResponseHandler error a =
-  Http.Response -> String -> Task (Error error) a
+    Http.Response -> String -> Task (Error error) a
 
 
 handleResponse : ResponseHandler error a -> ResponseHandler error a -> Http.Response -> Task (Error error) a
 handleResponse onSuccess onError response =
-  let
-    unexpectedPayloadError =
-      HttpError (Http.UnexpectedPayload "Response body is a blob, expecting a string.")
-  in
-    case 200 <= response.status && response.status < 300 of
-      True ->
-        case response.value of
-          Http.Text str ->
-            onSuccess response str
+    let
+        unexpectedPayloadError =
+            HttpError (Http.UnexpectedPayload "Response body is a blob, expecting a string.")
+    in
+        case 200 <= response.status && response.status < 300 of
+            True ->
+                case response.value of
+                    Http.Text str ->
+                        onSuccess response str
 
-          _ ->
-            fail unexpectedPayloadError
+                    _ ->
+                        fail unexpectedPayloadError
 
-      False ->
-        case response.value of
-          Http.Text str ->
-            onError response str
+            False ->
+                case response.value of
+                    Http.Text str ->
+                        onError response str
 
-          _ ->
-            fail unexpectedPayloadError
+                    _ ->
+                        fail unexpectedPayloadError
 
 
 {-| If there was a `<meta name="csrf-token">` tag in the page's `<head>` when
@@ -211,9 +211,9 @@ handleResponse onSuccess onError response =
 -}
 csrfToken : Result String String
 csrfToken =
-  Native.Rails.csrfToken
+    Native.Rails.csrfToken
 
 
 (=>) : a -> a -> ( a, a )
 (=>) =
-  (,)
+    (,)
