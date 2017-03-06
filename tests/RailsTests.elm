e6cfbfc596bb02cf7159bbd65f4178a8861a0f41
Aaron VonderHaar <av@noredink.com>
2017-03-06 12:05:21 -0800

    Add tests for Rails.decodeErrors

--- tests/RailsTests.elm
+++ tests/RailsTests.elm
@@ -2,9 +2,42 @@ module RailsTests exposing (all)
 
 import Test exposing (..)
 import Expect exposing (Expectation)
+import Rails
+import Json.Decode
+import Http
+import Dict
 
 
 all : Test
 all =
     describe "Rails"
-        []
+        [ describe "decodeErrors"
+            [ test "Passes through Ok values" <|
+                \() ->
+                    Ok ()
+                        |> Rails.decodeErrors Json.Decode.string
+                        |> Expect.equal (Ok ())
+            , test "Parses the body of BadStatus errors" <|
+                \() ->
+                    Err
+                        (Http.BadStatus
+                            { url = ""
+                            , status = { code = 400, message = "" }
+                            , headers = Dict.empty
+                            , body = "\"custom error\""
+                            }
+                        )
+                        |> Rails.decodeErrors Json.Decode.string
+                        |> expectErr (.rails >> Expect.equal (Just "custom error"))
+            ]
+        ]
+
+
+expectErr : (x -> Expectation) -> Result x a -> Expectation
+expectErr check result =
+    case result of
+        Err x ->
+            check x
+
+        Ok _ ->
+            Expect.fail ("Expected (Err _), but got: " ++ toString result)
