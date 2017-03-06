2fde43c1a55f77dff4581e27a7e2393d5928cad3
Aaron VonderHaar <av@noredink.com>
2017-03-06 11:55:02 -0800

    Set up elm-test

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+if [ ! -e node_modules/.bin/elm-test ]; then
+    npm install elm-test
+fi
+
 set -ex
 
+node_modules/.bin/elm-test
 elm-make --yes
 (cd examples; elm-make --yes Main.elm)
---
+++ tests/RailsTests.elm
@@ -0,0 +1,10 @@
+module RailsTests exposing (all)
+
+import Test exposing (..)
+import Expect exposing (Expectation)
+
+
+all : Test
+all =
+    describe "Rails"
+        []
